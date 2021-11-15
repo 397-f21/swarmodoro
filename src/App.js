1b400bbcfdf05c75016dcedfbad8e40bf101caa7
KailaiTang <56147372+KailaiTang@users.noreply.github.com>
2021-11-14 20:09:42 -0600

    initial timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { useTimer } from 'react-timer-hook';
 
-function App() {
+function MyTimer({ expiryTimestamp }) {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1> Swarmodoro </h1>
+      <p>A perfect swarm timer</p>
+      <div style={{ fontSize: '100px' }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Swarming...' : 'Next person!'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button>
+      <button onClick={() => {
+        // Restarts to 5 minutes timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 3);
+        restart(time)
+      }}>Restart</button>
     </div>
+  );
+}
+
+const App = () => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+  return (
+    <>
+      <div>
+        <MyTimer expiryTimestamp={time} />
+      </div>
+
+      <div class="md-form">
+        <input type="text" id="manual-operations-input" class="form-control" placeholder="Now"/>
+        <label for="form1" class ="">Check the minutes</label>
+      </div>
+
+    </>
   );
 }
 
